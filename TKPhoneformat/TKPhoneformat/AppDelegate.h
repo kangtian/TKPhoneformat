c9592970f214b20f02d68cfa2ebfa450c48ef107
悠悠岁月m <18823177001@163.com>
2017-09-12 09:50:18 +0800

    初始化工程

---
+++ TKPhoneformat/TKPhoneformat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TKPhoneformat
+//
+//  Created by pro1 on 2017/9/12.
+//  Copyright © 2017年 kangshifu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
